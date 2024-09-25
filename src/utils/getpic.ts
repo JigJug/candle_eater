a75e82499ff700053f7b841aea4f0b316990d1ae
JigJug <easybot@tutanota.com>
2024-09-25 15:54:43 +0100

    tidyup

--- src/utils/getpic.ts
+++ src/utils/getpic.ts
@@ -7,17 +7,12 @@ export async function getPicWithBrowser(url: string){
 
   const {page, browser} = await launchBrowser(true)
 
-  await page.goto(url)
+  await page.goto(url, {waitUntil: "domcontentloaded"})
 
   // Block certain requests to avoid loading unnecessary popups
   await page.setRequestInterception(true);
-
   blockRequests(page)
-
-  await new Promise(r => setTimeout(r, 5000));
-
-
-  getRidOfCookiesPopup(page);
+  await getRidOfCookiesPopup(page);
 
   // Wait for the element to be visible
   await page.waitForSelector(chartSelector); // or use a more specific selector if necessary
@@ -26,62 +21,6 @@ export async function getPicWithBrowser(url: string){
   const chartelement = await page.$(chartSelector);
 
   if (!chartelement) return null//console.log('Element has no bounding box!');
-  // Get the element's bounding box (position and size)
-  /*const boundingBox = await element.boundingBox();
-      
-  if (!boundingBox) return null//console.log('Element not found!');
-  
-  const x = boundingBox.x + boundingBox.width / 2;  // Center of the element
-  const y = boundingBox.y + boundingBox.height / 2; // Center of the element
-
-  // Move the mouse to the center of the element
-  await page.mouse.move(x, y);
-
-  //await page.mouse.click(x,y)
-
-  // Press and hold the 'Ctrl' key
-  //await page.keyboard.down('Control');
-
-  await new Promise(r => setTimeout(r, 2000));*/
-
-  // Perform double mouse scroll (scroll down twice)
-  /**await page.mouse.wheel({ deltaY: 200 }); // Scroll downawait page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll downawait page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  await page.mouse.wheel({ deltaY: 200 }); // Scroll down
-  */
-
-  /*await page.evaluate(()=>{
-    const chartSelector1 = 'body > div.js-rootresizer__contents.layout-with-border-radius > div.layout__area--center > div.chart-container.single-visible.top-full-width-chart.active'
-    const chartEle = document.querySelector(chartSelector1);
-    if(chartEle) chartEle.scrollBy(0, 800)
-  })*/
-
-  //await page.mouse.down()
-  //await page.mouse.drag({x,y}, {x:x+300,y:0})
-  //await page.mouse.up();
-
-  // Press and hold the 'Ctrl' key
-  //await page.keyboard.up('Control');
-
-  // Enable drag interception
-  //await page.setDragInterception(true);
 
   await page.waitForSelector(".time-axis")
 
@@ -89,73 +28,11 @@ export async function getPicWithBrowser(url: string){
 
   timeaxisele?.drag({x: 300, y:0})
 
-  await new Promise(r => setTimeout(r, 4000));
-  
-  // Simulate pressing Ctrl + Shift + S
-  //await page.keyboard.down('Control');
-  //await page.keyboard.down('Alt');
-  //await page.keyboard.press('S');
-  //await page.keyboard.up('Alt');
-  //await page.keyboard.up('Control');
+  await new Promise(r => setTimeout(r, 1000));
 
   const screenshot = await chartelement.screenshot({type:"png"})
   console.log('Screenshot saved pic ');
   await new Promise(r => setTimeout(r, 1000));
-
-  /**await page.waitForSelector("body > div.js-rootresizer__contents.layout-with-border-radius > div.layout__area--top > div > div > div:nth-child(3) > div.wrapOverflow-wXGVFOC9 > div > div > div > div > div:nth-child(16) > button.button-merBkM5y.apply-common-tooltip.accessible-merBkM5y")
-
-  const elementSnap = await page.$("body > div.js-rootresizer__contents.layout-with-border-radius > div.layout__area--top > div > div > div:nth-child(3) > div.wrapOverflow-wXGVFOC9 > div > div > div > div > div:nth-child(16) > button.button-merBkM5y.apply-common-tooltip.accessible-merBkM5y")
-
-  elementSnap?.click()
-  await page.keyboard.press('ArrowDown'); // Move down the menu
-  await page.keyboard.press('Enter'); // Select the highlighted item
-  console.log('Screenshot saved pic using selector and key down');
-  await new Promise(r => setTimeout(r, 10000));
-
-  // Grant clipboard access permission
-  const context = browser.defaultBrowserContext();
-  await context.overridePermissions('https://www.tradingview.com', ['clipboard-read']);
-
-
-  const imageData = await page.evaluate(() => {
-      return navigator.clipboard.read().then(clipboardItems => {
-          const clipboardItem = clipboardItems[0];
-          const availableTypes = clipboardItem.types;
-  
-          if (!availableTypes.includes('image/png')) {
-              throw new Error('Clipboard does not contain an image of type png');
-          }
-  
-          return clipboardItem.getType('image/png').then(blob => {
-              return new Promise((resolve) => {
-                  const reader = new FileReader();
-                  reader.onload = () => resolve(reader.result); // Base64 string
-                  reader.readAsDataURL(blob);
-              });
-          });
-      });
-  });
-  
-  
-  
-
-  // Type assertion to handle 'unknown' type
-  if (typeof imageData === 'string') {
-      // Convert Base64 image to Buffer
-      const base64Data = imageData.split(',')[1];
-      const imageBuffer = Buffer.from(base64Data, 'base64');
-
-      console.log(imageBuffer)
-
-      //return imageBuffer
-
-      // Send the image via Telegram using Grammy
-      //await bot.api.sendPhoto('<CHAT_ID>', { source: imageBuffer }); // Replace with actual chat ID
-
-      //console.log('Image sent to Telegram!');
-  } else {
-      console.error('Failed to retrieve image data from clipboard');
-  }**/
 
   page.removeAllListeners();
 
@@ -163,9 +40,6 @@ export async function getPicWithBrowser(url: string){
   await browser.close();
 
   return screenshot
-
-
-
 };
 
 
